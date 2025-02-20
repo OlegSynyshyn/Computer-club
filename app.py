05d8a800e9b5e978575e547be29b0693041ff9eb
Oleh Synyshyn <oleg.synyshyn10@gmail.com>
2025-02-20 17:51:56 +0200

    20.02

--- app.py
+++ app.py
@@ -1,3 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from db_scripts import DBManager
+from dotenv import load_dotenv
+load_dotenv()
 import os
+from datetime import datetime
+
+IMG_PATH = os.path.dirname(__file__) + os.sep + 'static' + os.sep + 'Photos'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db = DBManager("clubcom.db")
+
+@app.route("/")
+def index():
+    categories = db.get_categories()
+    articles = db.get_articles()
+    return render_template("index.html", categories=categories, articles=articles)
+
+@app.route("/category/<int:category_id>")
+def category_page(category_id):
+    categories = db.get_categories()
+    articles = db.get_articles_by_category(category_id)
+    return render_template("category.html", categories=categories, articles=articles, category_name=categories[category_id-1][1])
+
+@app.route("/articles/<int:article_id>")
+def article_page(article_id):
+    categories = db.get_categories()
+    article = db.get_article_by_id(article_id)
+    return render_template("article.html", categories=categories, article=article)
+
+@app.route("/articles/new", methods=["GET", "POST"])
+def new_article():
+    categories = db.get_categories()
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        content = request.form['text']
+        image = request.files['image']
+        image.save(IMG_PATH + os.sep + image.filename)
+        category_id = request.form['category']
+        user_id = 1  # Припустимо, що користувач з ID 1 додає статтю
+        db.create_article(user_id, category_id, title, content, image.filename)
+        return redirect(url_for('index'))
+    return render_template("new_article.html", categories=categories)
+
+@app.route("/bookings/new", methods=["GET", "POST"])
+def new_booking():
+    if request.method == 'POST':
+        user_id = 1  # Припустимо, що користувач з ID 1 робить запис
+        start_time = request.form['start_time']
+        db.create_booking(user_id, start_time)
+        return redirect(url_for('index'))
+    return render_template("new_booking.html")
+
+@app.route("/search")
+def search_page():
+    categories = db.get_categories()
+    query = request.args.get("query", '')
+    articles = db.search_articles(query)
+    return render_template("category.html", categories=categories, articles=articles, category_name=query)
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
---
+++ db_scripts.py
@@ -0,0 +1,61 @@
+import sqlite3
+from datetime import datetime
+
+class DBManager():
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+
+    def open_db(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+    
+    def get_categories(self):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM categories''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_articles(self):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+
+    def get_articles_by_category(self, category_id):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles WHERE category_id=?''', [category_id])
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_article_by_id(self, article_id):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles WHERE id=?''', [article_id])
+        data = self.cursor.fetchone()
+        self.conn.close()
+        return data
+    
+    def create_article(self, user_id, category_id, title, content, image):
+        self.open_db()
+        self.cursor.execute('''INSERT INTO articles (user_id, category_id, title, content, created_at) VALUES (?, ?, ?, ?, ?)''', 
+                            [user_id, category_id, title, content, datetime.now()])
+        self.conn.commit()
+        self.conn.close()
+
+    def create_booking(self, user_id, start_time):
+        self.open_db()
+        self.cursor.execute('''INSERT INTO bookings (user_id, start_time, status, created_at) VALUES (?, ?, ?, ?)''', 
+                            [user_id, start_time, 'pending', datetime.now()])
+        self.conn.commit()
+        self.conn.close()
+
+    def search_articles(self, query):
+        self.open_db()
+        query = '%' + query + '%'
+        self.cursor.execute('''SELECT * FROM articles WHERE title LIKE ? OR content LIKE ?''', [query, query])
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
